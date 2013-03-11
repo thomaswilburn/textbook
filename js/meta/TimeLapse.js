0a645af7827e3352abe6171ce07f12cf0e12cefd
Thomas Wilburn <news@thomaswilburn.net>
2013-03-10 23:03:33 -0700

    Timelapse tool imports and exports diffs.
    Also small edits on object and array chapters.

--- js/meta/TimeLapse.js
+++ js/meta/TimeLapse.js
@@ -87,6 +87,83 @@ define(['jquery'], function() {
         }
       }
       return revisions;
+    },
+    serialize: function() {
+      var id = 0;
+      var corpus = this.revisions.map(function(item,revision) {
+        var split = item.text.split(/\r?\n\r?/);
+        for (var i = 0; i < split.length; i++) {
+          split[i] = {
+            line: i,
+            text: split[i],
+          }
+        }
+        return split;
+      });
+      //create starter array from source, adding revision stamps
+      var list = corpus[0].slice();
+      list.forEach(function(item) {
+        item.start = 0;
+        item.end = Infinity;
+      });
+      /*
+      We continually iterate over and recreate the source array. It's not
+      efficient, but the linked list I tried earlier was kind of a drag to
+      update correctly.
+      */
+      for (var rev = 1; rev < corpus.length; rev++) {
+        var merged = [];
+        var revision = corpus[rev];
+        var target = 0;
+        list.forEach(function(item) {
+          //go through the revision looking for matches
+          var source = item.text;
+          var found = false;
+          for (var i = target; i < revision.length; i++) {
+            var line = revision[i].text;
+            //if we find a match...
+            if (line == source) {
+              found = true;
+              //merge in skipped lines as additions
+              while (i > target) {
+                var addition = revision[target];
+                addition.start = rev;
+                addition.end = Infinity;
+                merged.push(addition);
+                target++;
+              }
+              target++;
+              break;
+            }
+          }
+          //if not found, it was deleted
+          if (!found) {
+            item.end = rev - 1;
+          }
+          merged.push(item);
+        });
+        //if there's still stuff left in the revision, it's added
+        while (target < revision.length) {
+          var line = revision[target];
+          line.start = rev;
+          line.end = Infinity;
+          //console.log('add', line.text);
+          merged.push(line);
+          target++;
+        }
+        list = merged;
+      }
+      var tagged = merged.map(function(item) {
+        var tag = "";
+        if (item.start > 0 || item.end < Infinity) {
+          tag = "@" + item.start + ',';
+          tag += item.end == Infinity ? '' : item.end;
+          tag += "@";
+        }
+        return tag + item.text;
+      });
+      var output = tagged.join('\n');
+      return output;
     }
   };
 
